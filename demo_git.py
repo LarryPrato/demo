fcfa6f269ae256434aea34b7f51ae0b196ce6db9
LarryPrato <larryprato@gmail.com>
2023-08-24 23:12:59 -0300

    chore: testing new branch

--- demo_git.py
+++ demo_git.py
@@ -1,5 +1,9 @@
-def greeting(man):
+def greeting(man:str):
     print(f"Hello {man}, from github")
+
+def bye_bye(boy:str):
+    print(f'Bye {boy}')
 
 if __name__ == "__main":
     greeting("Mr. Bean")
+    bye_bye("Doe")
